e2d496863deb9aa9401f51c913f78fd222ea16da
gu4ripolo <el_fackoo@hotmail.com>
2023-02-14 22:02:43 -0600

    :sparkles: adding input validation scripts

---
+++ input_validation/idiot.py
@@ -0,0 +1,14 @@
+import pyinputplus as pyip
+
+'''
+while True:
+    prompt = "Want to know how to keep an idiot busy for hours?\n"
+    response = pyip.inputYesNo(prompt)
+
+    if response == 'no':
+        break
+print("Thank you. Have a nice day!")
+'''
+
+prompt = "¿Quieres saber cómo mantener ocupado a un idiota durante horas?\n" 
+response = pyip.inputYesNo(prompt, yesVal='si', noVal='no')
